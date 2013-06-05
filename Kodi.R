dd0314d7553dea75a2c17546f857be6be218a28b
Kodi Arfer <git@arfer.net>
2013-06-05 16:54:37 -0400

    Added 'slurp'.

--- Kodi.R
+++ Kodi.R
@@ -275,6 +275,9 @@ ucfirst = function(s)
         toupper(substring(s, 1, 1)),
         substring(s, 2)))
 
+slurp = function(filepath)
+    readChar(filepath, file.info(filepath)$size)
+
 # --------------------------------------------------
 # Numbers
 # --------------------------------------------------
